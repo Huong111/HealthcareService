fa19e46044d425ed69f778f22cbad5fc5ecddc39
Huong111 <heobela20@gmail.com>
2021-10-27 18:45:54 +0700

    cmm1

---
+++ src/main/java/Pages/LoginPage.java
@@ -0,0 +1,22 @@
+package Pages;
+
+import Locators.LoginLocators;
+import Objects.Account;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+    LoginLocators locators = new LoginLocators();
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    /**
+     * login function
+     * @param account
+     */
+    public void login(Account account) {
+        setText(locators.INPUT_USERNAME, account.getUsername());
+        setText(locators.INPUT_PASSWORD, account.getPassword());
+        click(locators.BUTTON_LOGIN);
+    }
+}
---
+++ src/main/java/Utils/Contans.java
@@ -0,0 +1,5 @@
+package Utils;
+
+class Constants {
+    public static String AUT_URL = "https://katalon-demo-cura.herokuapp.com/index.php";
+}
